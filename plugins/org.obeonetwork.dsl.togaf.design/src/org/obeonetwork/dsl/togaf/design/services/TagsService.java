aff91872f5f009f8496debc466d56cc6305f008e
sdrapeau <Stephane.Drapeau@obeo.fr>
2013-02-15 18:34:13 +0100

    Added Landscape support

---
+++ plugins/org.obeonetwork.dsl.togaf.design/src/org/obeonetwork/dsl/togaf/design/services/TagsService.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2012 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.obeonetwork.dsl.togaf.design.services;
+
+import org.eclipse.emf.ecore.EObject;
+import org.obeonetwork.dsl.togaf.design.tags.SemanticResourceSelectorHandler;
+
+/**
+ * Utility class for the Tags metamodel.
+ * 
+ * @author SDrapeau
+ * 
+ */
+public class TagsService {
+
+	/**
+	 * Get the root element of the semantic model.
+	 * 
+	 * @param e
+	 *            not used.
+	 * @return
+	 */
+	public EObject getRootElement(EObject e) {
+		if (SemanticResourceSelectorHandler.getSemanticResourceSelector() != null) {
+			return SemanticResourceSelectorHandler
+					.getSemanticResourceSelector().getRootElement();
+		}
+		return null;
+	}
+
+}
