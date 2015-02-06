45bbf7981cf4c0870f5a15b06e808223f8ad89b7
Julien Dupont <julien.dupont@obeo.fr>
2015-02-06 15:52:41 +0100

    [Togaf-Designer] ODFOREA-1684 Faire une nouvelle représentation Logical Component Functional Scope

--- plugins/org.obeonetwork.dsl.togaf.design/src/org/obeonetwork/dsl/togaf/design/services/ApplicationArchitectureServices.java
+++ plugins/org.obeonetwork.dsl.togaf.design/src/org/obeonetwork/dsl/togaf/design/services/ApplicationArchitectureServices.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.obeonetwork.dsl.togaf.contentfwk.contentfwk.BusinessService;
 import org.obeonetwork.dsl.togaf.contentfwk.contentfwk.Function;
 import org.obeonetwork.dsl.togaf.contentfwk.contentfwk.LogicalApplicationComponent;
 import org.obeonetwork.dsl.togaf.contentfwk.contentfwk.Service;
@@ -16,22 +17,59 @@ import org.obeonetwork.dsl.togaf.contentfwk.contentfwk.Service;
  * VSM services for the ApplicationArchitectures elements.
  * 
  * @author jdupont
- *
+ * 
  */
 public class ApplicationArchitectureServices {
 
-	public List<Function> getFunctionsLinkedToLogicalApplication(LogicalApplicationComponent context){
-		List<Function> functions = new ArrayList<Function>();
-		for (Service service : context.getImplementsServices()){
-			if (service.getProvidesGovernedInterfaceToAccessFunctions().size() > 0){
-				functions.addAll(service.getProvidesGovernedInterfaceToAccessFunctions());
-			}			
+	public List<Function> getFunctionsLinkedToLogicalApplication(
+			LogicalApplicationComponent context) {
+		Set<Function> functions = new HashSet<Function>();
+		for (Service service : context.getImplementsServices()) {
+			if (service.getProvidesGovernedInterfaceToAccessFunctions().size() > 0) {
+				functions.addAll(service
+						.getProvidesGovernedInterfaceToAccessFunctions());
+			}
 		}
-		//Remove duplicate
-		Set<Function> set = new HashSet<Function>() ;
-        set.addAll(functions) ;
-        ArrayList<Function> functionsWithoutDuplicates = new ArrayList<Function>(set) ;
+		ArrayList<Function> functionsWithoutDuplicates = new ArrayList<Function>(
+				functions);
 		return functionsWithoutDuplicates;
 	}
-		
+
+	/**
+	 * Check is function is bounded by services type of BusinessService.
+	 * 
+	 * @param context
+	 *            a function
+	 * @return boolean true if the function is bounded by services type of
+	 *         BusinessService false otherwise
+	 */
+	public boolean isBoundedByBusinessServices(Function context) {
+		boolean isBoundedByBusinessServices = false;
+		for (Service service : context.getIsBoundedByServices()) {
+			if (service instanceof BusinessService) {
+				isBoundedByBusinessServices = true;
+				break;
+			}
+		}
+		return isBoundedByBusinessServices;
+	}
+
+	/**
+	 * Return list of business services that bounded the function passed to
+	 * parameters
+	 * 
+	 * @param context
+	 *            a function
+	 * @return list of business services that bounded the function
+	 */
+	public List<BusinessService> getBusinessServices(Function context) {
+		List<BusinessService> businessServices = new ArrayList<BusinessService>();
+		for (Service service : context.getIsBoundedByServices()) {
+			if (service instanceof BusinessService) {
+				businessServices.add((BusinessService) service);
+			}
+		}
+		return businessServices;
+	}
+
 }
