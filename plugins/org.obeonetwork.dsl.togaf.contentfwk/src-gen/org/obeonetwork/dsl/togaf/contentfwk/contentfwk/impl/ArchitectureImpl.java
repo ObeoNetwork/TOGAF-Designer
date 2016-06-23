1cf0ed3d20049c0cadac30ebeb5084bca64b76f4
Maxime Porhel <maxime.porhel@obeo.fr>
2016-06-23 14:04:02 +0200

    Improve load performances on big models.
    . Introduce a specific resource implementation with several load options
    . Update the id to EObject map when the id is changed. 
    . Register the resource for togaf and contentfwk file extensions
    . Add togaf in the extensions handlded by the editor

--- plugins/org.obeonetwork.dsl.togaf.contentfwk/src-gen/org/obeonetwork/dsl/togaf/contentfwk/contentfwk/impl/ArchitectureImpl.java
+++ plugins/org.obeonetwork.dsl.togaf.contentfwk/src-gen/org/obeonetwork/dsl/togaf/contentfwk/contentfwk/impl/ArchitectureImpl.java
@@ -12,6 +12,7 @@
 package org.obeonetwork.dsl.togaf.contentfwk.contentfwk.impl;
 
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.internal.cdo.CDOObjectImpl;
 import org.obeonetwork.dsl.togaf.contentfwk.contentfwk.Architecture;
 import org.obeonetwork.dsl.togaf.contentfwk.contentfwk.ContentfwkPackage;
@@ -25,135 +26,148 @@ import org.obeonetwork.dsl.togaf.contentfwk.contentfwk.util.IdUtil;
  * <p>
  * The following features are implemented:
  * <ul>
- *   <li>{@link org.obeonetwork.dsl.togaf.contentfwk.contentfwk.impl.ArchitectureImpl#getID <em>ID</em>}</li>
+ * <li>
+ * {@link org.obeonetwork.dsl.togaf.contentfwk.contentfwk.impl.ArchitectureImpl#getID
+ * <em>ID</em>}</li>
  * </ul>
  * </p>
  *
  * @generated
  */
-public abstract class ArchitectureImpl extends CDOObjectImpl implements
-		Architecture {
-	/**
-	 * The default value of the '{@link #getID() <em>ID</em>}' attribute. <!--
-	 * begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @see #getID()
-	 * @generated
-	 * @ordered
-	 */
-	protected static final String ID_EDEFAULT = null;
+public abstract class ArchitectureImpl extends CDOObjectImpl implements Architecture {
+    /**
+     * The default value of the '{@link #getID() <em>ID</em>}' attribute. <!--
+     * begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @see #getID()
+     * @generated
+     * @ordered
+     */
+    protected static final String ID_EDEFAULT = null;
 
-	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated NOT
-	 */
-	protected ArchitectureImpl() {
-		super();
-		if (ContentfwkPreferences.getElementIdInitializationPolicy() == ElementIdInitializationPolicy.CREATION_TIME) {
-			forceID(IdUtil.generateId());
-		}
-	}
+    /**
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @generated NOT
+     */
+    protected ArchitectureImpl() {
+        super();
+        if (ContentfwkPreferences.getElementIdInitializationPolicy() == ElementIdInitializationPolicy.CREATION_TIME) {
+            forceID(IdUtil.generateId());
+        }
+    }
 
-	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	protected EClass eStaticClass() {
-		return ContentfwkPackage.Literals.ARCHITECTURE;
-	}
+    /**
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @generated
+     */
+    @Override
+    protected EClass eStaticClass() {
+        return ContentfwkPackage.Literals.ARCHITECTURE;
+    }
 
-	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	protected int eStaticFeatureCount() {
-		return 0;
-	}
+    /**
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @generated
+     */
+    @Override
+    protected int eStaticFeatureCount() {
+        return 0;
+    }
 
-	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * @generated
-	 */
-	public String getID() {
-		return (String)eDynamicGet(ContentfwkPackage.ARCHITECTURE__ID, ContentfwkPackage.Literals.ARCHITECTURE__ID, true, true);
-	}
+    /**
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @generated
+     */
+    @Override
+    public String getID() {
+        return (String) eDynamicGet(ContentfwkPackage.ARCHITECTURE__ID, ContentfwkPackage.Literals.ARCHITECTURE__ID, true, true);
+    }
 
-	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated NOT
-	 */
-	public void setID(String newID) {
-		if(IdUtil.canIdBeSet(eResource(), newID, cdoID())){
-			forceID(newID);
-		}
-	}
+    /**
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @generated NOT
+     */
+    @Override
+    public void setID(String newID) {
+        Resource eResource = eResource();
+        String oldID = getID();
+        if (IdUtil.canIdBeSet(eResource, newID, cdoID())) {
+            forceID(newID);
+            IdUtil.updateResourceIds(eResource, oldID, newID);
+        }
+    }
 
-	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated NOT
-	 */
-	public void forceID(String newID) {
-		eDynamicSet(ContentfwkPackage.ARCHITECTURE__ID,
-				ContentfwkPackage.Literals.ARCHITECTURE__ID, newID);
-	}
+    /**
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @generated NOT
+     */
+    @Override
+    public void forceID(String newID) {
+        eDynamicSet(ContentfwkPackage.ARCHITECTURE__ID, ContentfwkPackage.Literals.ARCHITECTURE__ID, newID);
+    }
 
-	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public Object eGet(int featureID, boolean resolve, boolean coreType) {
-		switch (featureID) {
-			case ContentfwkPackage.ARCHITECTURE__ID:
-				return getID();
-		}
-		return super.eGet(featureID, resolve, coreType);
-	}
+    /**
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @generated
+     */
+    @Override
+    public Object eGet(int featureID, boolean resolve, boolean coreType) {
+        switch (featureID) {
+        case ContentfwkPackage.ARCHITECTURE__ID:
+            return getID();
+        }
+        return super.eGet(featureID, resolve, coreType);
+    }
 
-	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public void eSet(int featureID, Object newValue) {
-		switch (featureID) {
-			case ContentfwkPackage.ARCHITECTURE__ID:
-				setID((String)newValue);
-				return;
-		}
-		super.eSet(featureID, newValue);
-	}
+    /**
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @generated
+     */
+    @Override
+    public void eSet(int featureID, Object newValue) {
+        switch (featureID) {
+        case ContentfwkPackage.ARCHITECTURE__ID:
+            setID((String) newValue);
+            return;
+        }
+        super.eSet(featureID, newValue);
+    }
 
-	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public void eUnset(int featureID) {
-		switch (featureID) {
-			case ContentfwkPackage.ARCHITECTURE__ID:
-				setID(ID_EDEFAULT);
-				return;
-		}
-		super.eUnset(featureID);
-	}
+    /**
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @generated
+     */
+    @Override
+    public void eUnset(int featureID) {
+        switch (featureID) {
+        case ContentfwkPackage.ARCHITECTURE__ID:
+            setID(ID_EDEFAULT);
+            return;
+        }
+        super.eUnset(featureID);
+    }
 
-	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public boolean eIsSet(int featureID) {
-		switch (featureID) {
-			case ContentfwkPackage.ARCHITECTURE__ID:
-				return ID_EDEFAULT == null ? getID() != null : !ID_EDEFAULT.equals(getID());
-		}
-		return super.eIsSet(featureID);
-	}
+    /**
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @generated
+     */
+    @Override
+    public boolean eIsSet(int featureID) {
+        switch (featureID) {
+        case ContentfwkPackage.ARCHITECTURE__ID:
+            return ID_EDEFAULT == null ? getID() != null : !ID_EDEFAULT.equals(getID());
+        }
+        return super.eIsSet(featureID);
+    }
 
 } // ArchitectureImpl
